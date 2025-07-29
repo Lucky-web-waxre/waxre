b369fb9749bdf645e308742d5fd35dce17c840c5
Lucky-web-waxre <waxre060914@hotmail.com>
2025-07-30 06:37:23 +0800

    Update Click.js

--- 新建文件夹/Click.js
+++ 新建文件夹/Click.js
@@ -13,7 +13,7 @@ function login(){
 	var password = document.getElementById("login-password").value;
 	
 	if(user.get(username)==password){
-		window.location.href="https://github.com/Lucky-web-waxre/waxre/blob/main/%E5%BE%85%E5%BC%80%E6%94%BE.html";
+		window.location.href="/待开放.html";
 	}else{
 		alert("用户名或密码错误");
 	}
